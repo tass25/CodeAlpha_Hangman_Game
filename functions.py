18b7545bbc2d310906eb151b4b78d4966d4d304e
tass <laboutesse@gmail.com>
2024-04-01 12:22:52 +0200

    added display-word function

--- functions.py
+++ functions.py
@@ -5,3 +5,12 @@ words = ["hangman", "python", "computer", "programming", "keyboard", "mouse", "m
 
 def select_random_word():
     return random.choice(words)
+
+def display_word(word, guessed_letters):
+    displayed_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            displayed_word += letter + " "
+        else:
+            displayed_word += "_ "
+    return displayed_word.strip()
